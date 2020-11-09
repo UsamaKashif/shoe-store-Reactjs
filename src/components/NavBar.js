62979d2f7f2ae56bb768869e308edc3a103b2a6c
UsamaKashif <usamakashif1@gmail.com>
2020-11-09 22:27:12 +0500

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -21,6 +22,9 @@ const useStyles = makeStyles((theme) => ({
         letterSpacing: "0.05em",
         color: '#000000',
     },
+    link: {
+        textDecoration: "none"
+    }
 }));
 
 const NavBar = () => {
@@ -29,9 +33,11 @@ const NavBar = () => {
         <div className={classes.root}>
             <AppBar position="static" color="transparent">
                 <Toolbar>
-                    <Typography variant="h4" className={classes.title}>
-                        The Shoe Store
-                    </Typography>
+                    <Link to="/" className={classes.link}>
+                        <Typography variant="h4" className={classes.title}>
+                            The Shoe Store
+                        </Typography>
+                    </Link>
                 </Toolbar>
             </AppBar>
         </div>
